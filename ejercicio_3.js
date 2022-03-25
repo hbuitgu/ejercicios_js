96b739682556555b57d02f577b4fcd3d96350c56
hbuitgu <hbuitgu@gmail.com>
2022-03-25 00:15:54 -0500

    se resuelven los tres ejercicios del taller

--- ejercicio_3.js
+++ ejercicio_3.js
@@ -7,60 +7,34 @@
 // }
 // Cree un programa que permita recibir 50 alimentos diferentes y mediante una función primaria después de 5 segundos se  pueda obtener solo los alimentos de tipo vegetal que entreguen mas de 200 unidades de energía. Finalmente, una función callback debe permitir entregar la sumatoria de niveles de energía entregados por los alimentos vegetales consumidos en la dieta de Grogu.
 
-let alimentos = [
-  //vegetales
-  { nombre: "Wasabi", tipo: "	", cantidad_KCL: 309 },
-  {
-    nombre: "Olivas verdes / Aceitunas verdes",
-    tipo: "CALÓRICA VEGETABLES",
-    cantidad_KCL: 300,
-  },
-  {
-    nombre: "Ocra / Quimbombó",
-    tipo: "CALÓRICA VEGETABLES",
-    cantidad_KCL: 220,
-  },
-  { nombre: "Espárragos", tipo: "", cantidad_KCL: 223 },
-  //PASTAS
-  { nombre: "Fideos cocidos	", tipo: "CALÓRICA PASTA", cantidad_KCL: 142 },
-  {
-    nombre: "Pasta de espelta, cocida",
-    tipo: "CALÓRICA PASTA",
-    cantidad_KCL: 128,
-  },
-  { nombre: "Farfalle cocidas	", tipo: "CALÓRICA PASTA", cantidad_KCL: 135 },
-  { nombre: "Tallarines cocidos	", tipo: "CALÓRICA PASTA", cantidad_KCL: 147 },
-  {
-    nombre: "Fideos transparentes cocidos",
-    tipo: "CALÓRICA PASTA",
-    cantidad_KCL: 159,
-  },
-  {
-    nombre: "Espaguetis integrales cocidos",
-    tipo: "CALÓRICA PASTA",
-    cantidad_KCL: 124,
-  },
-  { nombre: "Albaricoque", tipo: "CALÓRICA PASTA", cantidad_KCL: 243 },
-];
+let nombre_alimentos = ["Wasabi","Olivas verdes / Aceitunas verdes","Ocra / Quimbombó","Espárragos","Fideos cocidos","Pasta de espelta, cocida","Espaguetis integrales cocidos","Albaricoque"];
+let tipo_alimentos=["vegetales", "carne", "pasta"]
 
-function filtro_alimentos_vegetales() {
-  setTimeout(function () {
-    let filtrado_alimento = alimentos.filter(function (alimento) {
-      return (
-        alimento.tipo == "CALÓRICA VEGETABLES" && alimento.cantidad_KCL >= 200
-      );
-    });
-    console.log(filtrado_alimento);
-  }, 5000);
+let alimentos= []
+for (i =0; i < 50; i++){
+  let alimento ={}
+  alimento.nombre_alimentos=(nombre_alimentos[Math.floor(Math.random()*nombre_alimentos.length)])
+  alimento.tipo_alimentos=(tipo_alimentos[Math.floor(Math.random()*tipo_alimentos.length)])
+  alimento.caloria=Math.floor(Math.random()*(min=100, max=500))
+alimentos.push(alimento)
 }
 
-function contar_calorias_vegetales() {
-  let suma = 0;
-  filtrado_alimento.forEach(function (alimento) {
-    suma += suma + alimento.filtrado_alimento.cantidad_KCL;
-    console.log(suma);
-  });
+function filtrar_vegetales (alimentos,callback){
+  setTimeout(function(){
+      let vegetales = alimentos.filter(function(alimento){
+          return(alimento.tipo_alimentos=="vegetales" && alimento.caloria>= 200)
+      })
+     callback (vegetales)
+  },5000)
+ 
 }
+filtrar_vegetales(alimentos,function(vegetales){
+  let  suma_calorias=0
+  vegetales.forEach(function(vegetales){
+    suma_calorias+=vegetales.caloria
+  })
+console.log(vegetales)
+console.log(`el tota de calorias es : ${suma_calorias}`)
+})
 
-filtro_alimentos_vegetales();
-contar_calorias_vegetales();
+
